b98a9c5b6774927599baa2d456b532d6032607ab
T-Kaito <take.off2357@gmail.com>
2024-09-10 05:35:26 +0900

    Hello world

--- Web/app/Http/Controllers/TestsController.php
+++ Web/app/Http/Controllers/TestsController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class TestsController extends Controller
 {
-    //
+    //testというviewファイルを表示して
+    public function test(){
+        return view('test');
+    }
 }
--- Web/routes/web.php
+++ Web/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('test');});
+Route::get('/test', [TsetsController::class,'test']);
